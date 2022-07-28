3f7156a8b42b01b21bcd9df7d6052395dd8b75e1
chriseteka <christopher.eteka@ing.com>
2022-07-28 13:21:29 +0200

    Initial commit

---
+++ src/main/java/com/chrisworks/practicecoderewrite/PracticeCodeRewriteApplication.java
@@ -0,0 +1,13 @@
+package com.chrisworks.practicecoderewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCodeRewriteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeCodeRewriteApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chrisworks/practicecoderewrite/PracticeCodeRewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisworks.practicecoderewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCodeRewriteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
